0ee98579b44c005db0733612eda20c7965c980a1
zavgm <zavgmaxden@mail.ru>
2023-02-18 18:03:02 +0300

    added newlines to task 7

--- Maks/ch1/Task7.java
+++ Maks/ch1/Task7.java
@@ -6,10 +6,10 @@ public class Task7 {
         Scanner scanner = new Scanner(System.in);
         final int normValue = Short.MAX_VALUE + 1;
         short a = (short) (scanner.nextInt() - normValue), b = (short) (scanner.nextInt() - normValue);
-        System.out.printf("Сумма: %d", a + normValue + b + normValue);
-        System.out.printf("Разность: %d", a - b);
-        System.out.printf("Умножение: %d", (a + normValue) * (b + normValue));
-        System.out.printf("Деление: %d", (a + normValue) / (b + normValue));
-        System.out.printf("Остаток от деления: %d", (a + normValue) % (b + normValue));
+        System.out.printf("Сумма: %d\n", a + normValue + b + normValue);
+        System.out.printf("Разность: %d\n", a - b);
+        System.out.printf("Умножение: %d\n", (a + normValue) * (b + normValue));
+        System.out.printf("Деление: %d\n", (a + normValue) / (b + normValue));
+        System.out.printf("Остаток от деления: %d\n", (a + normValue) % (b + normValue));
     }
 }
