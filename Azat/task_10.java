9de613822bcaec490e5ae53523a347ee8b8cc420
KIRIN17 <63730612+KIRIN17@users.noreply.github.com>
2023-02-26 19:22:52 +0300

    Delete Azat directory

--- Azat/task_10.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class task_10 {
-    public static void main(String[] args) {
-        Random rand = new Random();
-        System.out.print(Long.toUnsignedString(rand.nextLong(), 36));
-    }
-}
--- Azat/task_5.java
+++
@@ -1,8 +0,0 @@
-public class task_5 {
-    public static void main(String[] args) {
-        int number = (int) Double.MAX_VALUE;
-        System.out.println("Result:" +
-                "\nint:    " + number +
-                "\ndouble: " + Double.MAX_VALUE);
-    }
-}
